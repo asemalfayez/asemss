7f4f6eade7eba6626ee9707a6513e5397a3db9d1
asem <asemalfayez1@gmail.com>
2025-08-22 18:23:13 +0300

    firvb

--- sudoku-color-play-main/vite.config.ts
+++ sudoku-color-play-main/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: process.env.VITE_BASE_PATH || "/sudoku-color-play",
   server: {
     host: "::",
     port: 8080,
